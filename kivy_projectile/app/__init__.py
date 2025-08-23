7403c12a84aaf3fb12174d9910f6c7ee8b5a23fc
Divone3 <hamidreza.137606@gmail.com>
2025-08-24 00:17:23 +0330

    starting to add m3 theme._

--- kivy_projectile/app/__init__.py
+++ kivy_projectile/app/__init__.py
@@ -1,3 +1,3 @@
 from .app import BaseApp
 from .theme import BaseTheme, M3ThemableBehavior
-__all__ = ["BaseApp", "BaseTheme", "M3ThemableBehavior"]
+__all__ = [BaseApp, "BaseTheme", "M3ThemableBehavior"]
